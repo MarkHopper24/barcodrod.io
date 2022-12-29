1ebb0b53870a5e6d71267928065ab3831ffdacda
MarkHopper24 <106134632+MarkHopper24@users.noreply.github.com>
2022-12-29 12:19:54 -0600

    Delete barcodrod.io directory

--- barcodrod.io/Contracts/Services/ILocalSettingsService.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace barcodrod.io.Contracts.Services;
-
-public interface ILocalSettingsService
-{
-    Task<T?> ReadSettingAsync<T>(string key);
-
-    Task SaveSettingAsync<T>(string key, T value);
-}
--- barcodrod.io/Contracts/Services/IPageService.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace barcodrod.io.Contracts.Services;
-
-public interface IPageService
-{
-    Type GetPageType(string key);
-}
